63cf1b9df9f205b9083b03fa3c59431baed2168c
Phroton98 <joseph_salimin@hotmail.com>
2018-08-18 23:20:41 +0700

    remove token

--- migration/migration.go
+++ migration/migration.go
@@ -11,20 +11,18 @@ func CreateAllTable() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	db.DropTableIfExists(&order.OrderFlag{}, &order.OrderLocation{}, &order.Order{}, &driver.DriverLocation{})
+	
 	if !db.HasTable(&order.Order{}) {
-		db.DropTable(&order.Order{})
 		db.CreateTable(&order.Order{})
 	}
 	if !db.HasTable(&order.OrderLocation{}) {
-		db.DropTable(&order.OrderLocation{})
 		db.CreateTable(&order.OrderLocation{})
 	}
 	if !db.HasTable(&order.OrderFlag{}) {
-		db.DropTable(&order.OrderFlag{})
 		db.CreateTable(&order.OrderFlag{})
 	}
 	if !db.HasTable(&driver.DriverLocation{}) {
-		db.DropTable(&driver.DriverLocation{})
 		db.CreateTable(&driver.DriverLocation{})
 	}
 }
