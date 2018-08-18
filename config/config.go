02f2894c4701af3369b5758b93596e6624519560
Phroton98 <joseph_salimin@hotmail.com>
2018-08-18 17:49:53 +0700

    change to heroku setting

--- config/config.go
+++ config/config.go
@@ -27,4 +27,4 @@ const DB_NAME = "order"
 const ENVIRONMENT = "test"
 
 var DATABASE_URL = os.Getenv("DATABASE_URL")
-var REDIS_URL = "redis://h:pebfe2bd00d785fc79f982c9aaa72d063daf874e8c864e14ef52ef7e682213091@ec2-18-214-19-12.compute-1.amazonaws.com:26919"
+var REDIS_URL = `[redis://h:pebfe2bd00d785fc79f982c9aaa72d063daf874e8c864e14ef52ef7e682213091@ec2-18-214-19-12.compute-1.amazonaws.com]:26919`
